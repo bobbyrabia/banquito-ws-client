b5e19fd34b32f9cefe95bf6f70dfca5ab8227437
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-26 12:49:08 -0500

    Update CompanyController.java

--- src/main/java/ec/edu/espe/arquitectura/banquito/controller/CompanyController.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/controller/CompanyController.java
@@ -13,7 +13,7 @@ import ec.edu.espe.arquitectura.banquito.service.GroupCompanyService;
 
 @RestController
 @RequestMapping("/api/v2/companies")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class CompanyController {
     private final GroupCompanyService groupCompanyService;
 
