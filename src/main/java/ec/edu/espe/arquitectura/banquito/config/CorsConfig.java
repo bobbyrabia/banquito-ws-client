cd89bd4ecac77f76182003ba91554faac1552f2b
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-08-31 19:17:40 -0500

    fixed

--- src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
@@ -14,9 +14,10 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("https://striped-reserve-397605.web.app")
-                        .allowedOrigins("https://banquito-backoffice.web.app/")
-                        .allowedOrigins("https://banquito-bancaweb.web.app/")
-                        .allowedOrigins("https://banquito-tellerapp.web.app/")
+                        .allowedOrigins("https://banquito-backoffice.web.app")
+                        .allowedOrigins("https://banquito-bancaweb.web.app")
+                        .allowedOrigins("https://banquito-tellerapp.web.app")
+
                         .exposedHeaders("Content-Disposition")
                         .allowedMethods("*");
             }
