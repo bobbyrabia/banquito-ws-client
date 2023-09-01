9714cd8409389d0b22afb482beb8f489144a19c6
DayannaSilva <79052020+bobbyrabia@users.noreply.github.com>
2023-08-31 19:25:32 -0500

    Changes

--- src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
@@ -13,10 +13,8 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://striped-reserve-397605.web.app")
-                        .allowedOrigins("https://banquito-backoffice.web.app")
-                        .allowedOrigins("https://banquito-bancaweb.web.app")
-                        .allowedOrigins("https://banquito-tellerapp.web.app")
+                        .allowedOrigins("http://localhost:4200")
+
 
                         .exposedHeaders("Content-Disposition")
                         .allowedMethods("*");
