de577356f4ba84628400b706bc7ae3b3e5f50b1f
Roberto Freire <79052020+bobbyrabia@users.noreply.github.com>
2023-09-01 18:36:04 -0500

    exceptions.

--- src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
+++ src/main/java/ec/edu/espe/arquitectura/banquito/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/login")
-                        .allowedOrigins("https://striped-reserve-397605.web.app","https://banquito-backoffice.web.app/","https://banquito-bancaweb.web.app/","https://banquito-tellerapp.web.app/","http://localhost:4200")
+                        .allowedOrigins("https://striped-reserve-397605.web.app","https://banquito-backoffice-v2.web.app/","https://banquito-bancaweb-v2.web.app/","https://banquito-tellerapp-v2.web.app/","https://banquito-backoffice.web.app/","https://banquito-bancaweb.web.app/","https://banquito-tellerapp.web.app/","http://localhost:4200")
                         .exposedHeaders("*")
                         .allowedMethods("*");
                 registry.addMapping("/api/**")
